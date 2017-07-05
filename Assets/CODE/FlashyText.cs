bc1ba80403d7436a48ba2803dea54ab4bbd5420f
Kumilanka <kumilanka@gmail.com>
2017-07-06 00:03:06 +0300

    Initial commit: added a flashy title menu.

---
+++ Assets/CODE/FlashyText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach to an object with Unity UI Text component to create a flashing color cycle text effect. 
+/// Set color saturation for the text to >0 to see the effect. The more saturation, the more pronounced the effect.
+/// Set initial hue of text color in inspector to phase shift.
+/// </summary>
+
+[RequireComponent(typeof(UnityEngine.UI.Text))]
+public class FlashyText : MonoBehaviour
+{
+    [Range(0.1f, 3f)]
+    public float cycleTime = 1f;
+
+    private float time;
+    private float h, s, v;
+    private UnityEngine.UI.Text text;
+
+    void Start ()
+    {
+        // get the text component reference (that must exist)
+        text = GetComponent<UnityEngine.UI.Text>();
+
+        if (text != null) 
+        {
+            Color.RGBToHSV(text.color, out h, out s, out v);
+            // initially, set the cycle timer to current color phase (to allow for phase shift by editing color in inspector)
+            time = cycleTime * h;
+        }
+    }
+	
+	void Update ()
+    {
+        if (text != null)
+        {
+            Color.RGBToHSV(text.color, out h, out s, out v);
+
+            // run a simple timer
+            time -= Time.deltaTime;
+            if (time <= 0f)
+                time = cycleTime;
+
+            // cycle color saturation over time for that cool retro rainbow thing
+            h = Mathf.Lerp(0f, 1f, time / cycleTime);
+            text.color = Color.HSVToRGB(h, s, v);
+        }
+	}
+}
---
+++ Assets/CODE/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GameManager class to handle all the general gameplay stuff such as starting of the game, keeping score etc.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject startScreen;
+    [HideInInspector]
+    public bool game; // whether the game is running or not (to display title screen)
+
+	void Start ()
+    {
+        // pseudo-singleton for easy access
+        instance = this;
+	}
+	
+	void Update ()
+    {
+        if (!game)
+        {
+            startScreen.SetActive(true);
+            if (Input.anyKey)
+                game = true;
+        }
+
+        else
+            startScreen.SetActive(false);	
+	}
+}
