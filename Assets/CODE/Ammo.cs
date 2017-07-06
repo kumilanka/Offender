a9ea44580d5bd91f5a1d14cae65192b0d4ed8292
Kumilanka <kumilanka@gmail.com>
2017-07-06 13:26:50 +0300

    Added very wip movement and shooting mechanics for player

---
+++ Assets/CODE/Ammo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    public float lifetime = 3f;
+    private float r_lifetime;
+
+	void Start ()
+    {
+        r_lifetime = lifetime;
+	}
+	
+	void Update ()
+    {
+        // destroy bullet after lifetime has expired
+        r_lifetime -= Time.deltaTime;
+        if (r_lifetime <= 0f)
+            Destroy(gameObject);
+	}
+}
--- Assets/CODE/Player.cs
+++ Assets/CODE/Player.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public GameObject ship;
+    public GameObject ammoPrefab;
+    public Transform shootPosition;
+    public float shootVelocity;
+    private bool facingRight;
+    public float movementSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +17,29 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject ammo = Instantiate(ammoPrefab, shootPosition.position, Quaternion.identity);
+            ammo.GetComponent<Rigidbody2D>().velocity = new Vector2(shootVelocity * (facingRight ? 1f : -1f), 0f);
+        }
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(horizontal, vertical, 0f) * movementSpeed;
+
+        if (horizontal > 0f && !facingRight)
+        {
+            facingRight = true;
+            ship.transform.localScale = new Vector3(ship.transform.localScale.x * -1f, ship.transform.localScale.y, ship.transform.localScale.z);
+        }
+
+        else if (horizontal < 0f && facingRight)
+        {
+            facingRight = false;
+            ship.transform.localScale = new Vector3(ship.transform.localScale.x * -1f, ship.transform.localScale.y, ship.transform.localScale.z);
+        }
 	}
 }
